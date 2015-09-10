c774e8e77ba790ea97cf8563aa977c09fffb6be3
Jacobo Quintáns <jacoboqc@yahoo.es>
2015-09-10 23:13:22 +0200

    Js fully works and some more html tags

--- Práctica 1/p1.js
+++ Práctica 1/p1.js
@@ -1,16 +1,18 @@
-atrib=element.getElementById(form);
-if(document.getElementById(methp).checked){
-	atrib.setAttribute("method", "post");
+var atrib=document.getElementById("form");
+if(document.getElementById("methp").checked){
+	atrib.method="post";
 }else{
-	atrib.setAttribute("method", "get");
+	atrib.method="get";
 }
-if(document.getElementById(enca).checked){
-	atrib.setAttribute("enctype", "application/x-www-form-urlencoded");
+if(document.getElementById("enca").checked){
+	atrib.enctype="application/x-www-form-urlencoded";
 }else{
-	atrib.setAttribute("enctype", "multipart/form-data");
+	atrib.enctype="multipart/form-data";
 }
-if(document.getElementById(php1).checked){
-	atrib.setAttribute("action", "p1.php");
+if(document.getElementById("php1").checked){
+	atrib.action="p1.php";
 }else{
-	atrib.setAttribute("metodo", "/~sintprof/p1.php");
+	atrib.action="/~sintprof/p1.php";
 }
+var fecha = new Date();
+document.getElementById("hora").value=fecha.toString;
