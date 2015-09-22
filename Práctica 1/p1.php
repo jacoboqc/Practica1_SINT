94a510566bc73ac1deec6ad3e48485d94114b5cd
Jacobo Quintáns <jacoboqc@yahoo.es>
2015-09-22 23:49:32 +0200

    Correctly prints (almost) all fields

--- Práctica 1/p1.php
+++ Práctica 1/p1.php
@@ -1,31 +1,37 @@
 <html>
 	<head>
+	 	<meta charset="UTF-8">
 		<title>p1.php</title>
+		<meta name="author" content="Jacobo Quintáns Castro">
 		<link rel="stylesheet" type="text/css" href="p1.css">
 	</head>
 	<body>
 		<?php
 			if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-				$nombre=$_POST['nombre'];
-				$apellidos=$_POST['apellidos'];
-				$telef=$_POST['telef'];
-				$email=$_POST['email'];
-				$sexo=$_POST['sexo'];
-				$hora=$_POST['hora'];
-				$navegador=$_POST['navegador'];
+				$vars=$_POST;
 			} else {
-				$nombre=$_GET['nombre'];
-				$apellidos=$_GET['apellidos'];
-				$telef=$_GET['telef'];
-				$email=$_GET['email'];
-				$sexo=$_GET['sexo'];
-				$hora=$_POST['hora'];
-				$navegador=$_POST['navegador'];
+				$vars=$_GET;
 			}
-#			echo "$nombre\n$apellidos\n$telef\n$email\n$sexo\n$hora\n$navegador";
+			
+			function printVars ($field){
+				global $vars;
+			if($field=='sexo'){
+				if($vars[$field]=='m'){
+					$vars[$field]='Masculino';
+				}elseif($vars[$field]=='f'){
+					$vars[$field]='Femenino';
+				}else {$vars[$field]=NULL;}
+			}elseif($field=='mus'){
+				$musica=$vars[$field];
+			}
+			return	$vars[$field];
+			}
 		?>
 		<table>
 			<thead>
+				<tr>
+					<th colspan="2">Variables del formulario</th>
+				</tr>
 				<tr>
 					<th>Variable</th>
 					<th>Valor</th>
@@ -34,31 +40,81 @@
 			<tbody>
 				<tr>
 					<td>Nombre</td>
-					<td><?php echo $nombre; ?></td>
+					<td><?php echo printVars('nombre'); ?></td>
 				</tr>
 				<tr>
 					<td>Apellidos</td>
-					<td><?php echo $apellidos; ?></td>
+					<td><?php echo printVars('apellidos'); ?></td>
 				</tr>
 				<tr>
-					<td>Telefono</td>
-					<td><?php echo $telef; ?></td>
+					<td>Teléfono</td>
+					<td><?php echo printVars('telef'); ?></td>
 				</tr>
 				<tr>
 					<td>E-mail</td>
-					<td><?php echo $email; ?></td>
+					<td><?php echo printVars('email'); ?></td>
+				</tr>
+				<tr>
+					<td>Contraseña</td>
+					<td><?php echo printVars('pass'); ?></td>
 				</tr>
 				<tr>
 					<td>Sexo</td>
-					<td><?php echo $sexo; ?></td>
+					<td><?php echo printVars('sexo'); ?></td>
+				</tr>
+				<tr>
+					<td>Edad</td>
+					<td><?php echo (int) printVars('edad'); ?></td>
+				</tr>	
+				<tr>
+					<td>Música</td>
+					<td><?php echo printVars('mus'); ?></td>
+				</tr>
+				<tr>
+					<td>Información</td>
+					<td><?php echo printVars('info'); ?></td>
 				</tr>
 				<tr>
 					<td>Hora</td>
-					<td><?php echo $hora; ?></td>
+					<td><?php echo printVars('hora'); ?></td>
 				</tr>
 				<tr>
 					<td>Navegador</td>
-					<td><?php echo $navegador; ?></td>
+					<td><?php echo printVars('navegador'); ?></td>
+				</tr>
+			</tbody>
+		</table>
+		
+		<table>
+			<thead>
+				<tr>
+					<th colspan="2">Variables del servidor</th>
+				</tr>
+				<tr>
+					<th>Variable</th>
+					<th>Valor</th>
+				</tr>
+			</thead>
+			<tbody>
+				<tr>
+					<td>Servidor</td>
+					<td><?php echo $_SERVER["SERVER_NAME"]; ?></td>
+				</tr>
+				<tr>
+					<td>Direccion</td>
+					<td><?php echo $_SERVER["SERVER_ADDR"]; ?></td>
+				</tr>
+				<tr>
+					<td>Servidor</td>
+					<td><?php echo $_SERVER["SERVER_PORT"]; ?></td>
+				</tr>
+				<tr>
+					<td>Metodo</td>
+					<td><?php echo $_SERVER["REQUEST_METHOD"]; ?></td>
+				</tr>
+				<tr>
+					<td>Codificacion</td>
+					<td><?php echo $_SERVER["CONTENT_TYPE"]; ?></td>
 				</tr>
 			</tbody>
 		</table>
