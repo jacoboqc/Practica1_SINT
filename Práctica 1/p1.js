40daed819e3e6b5c1fcedbc74b510307095d8ad9
Jacobo Quintáns <jacoboqc@yahoo.es>
2015-10-01 18:24:33 +0200

    Checks patterns in js

--- Práctica 1/p1.js
+++ Práctica 1/p1.js
@@ -37,7 +37,7 @@ function toSubmit(){
 	getTime();
 	getBrowser();
 	var goRNot=true;
-	if(!mustMatch() || !terms() || !checkedMethEnc()){
+	if(!patterns() || !mustMatch() || !terms() || !checkedMethEnc()){
 		goRNot=false;
 	}
 	return goRNot;
@@ -90,6 +90,32 @@ function checkedMethEnc (){
 	if(!document.getElementById("php1").checked
 	&& !document.getElementById("phprof").checked){
 		alert("Debes seleccionar un destino");
+		ret = false;
+	}
+	return ret;
+	
+}
+
+function patterns(){
+	
+	var patName = new RegExp('^$|^[a-zA-Z\s]+$');
+	var patTelef = new RegExp('^$|^[9|8|7|6]\d{8}$');
+	var patMail = new RegExp('^$|^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,3})$');
+	var ret = true;
+	if(!patName.test(document.getElementById("nombre").value)){
+		alert("Introduzca un nombre correcto");
+		ret = false;
+	}
+	if(!patName.test(document.getElementById("apellidos").value)){
+		alert("Introduzca un apellido correcto");
+		ret = false;
+	}
+	if(!patTelef.test(document.getElementById("telef").value)){
+		alert("Introduzca un teléfono correcto");
+		ret = false;
+	}
+	if(!patMail.test(document.getElementById("email").value)){
+		alert("Introduzca un e-mail correcto");
 		ret = false;
 	}
 	return ret;
